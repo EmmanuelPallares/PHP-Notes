b6edf733e061d433f4f7605421a5fca8a2f95c0e
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-12-05 07:35:23 -0600

    calculating the iva and show with oop

--- 01.php
+++ 01.php
@@ -14,20 +14,48 @@ class Producto {
     }
 
 }
+class CalcularIva{
+ 
+  public function __construct(public int $numero, public int $total)
+    {
+    }
+public function calcularIva() {
+    $this->resultado = $this->numero * 0.16;
+    $this->totalconiba=$this->resultado+$this->numero;
+    echo "El iva es de: " . $this->resultado . " y el precio total es de: " . $this->totalconiba;
+}
+public function greeting() {
+    echo "   Vuelve Pronto";
+}
 
-$producto = new Producto('Tablet', 200, true);
-$producto->mostrarProducto();
+}
 
+
+$resultadoIva=new CalcularIva(100,100);
+
+
+$resultadoIva->calcularIva();
+$resultadoIva->greeting();
 
 echo "<pre>";
-var_dump($producto);
+var_dump($resultadoIva);
 echo "</pre>";
 
-$producto2 = new Producto('Monitor Curvo', 300, true);
-$producto2->mostrarProducto();
 
-echo "<pre>";
-var_dump($producto2);
-echo "</pre>";
+
+// $producto = new Producto('Tablet', 200, true);
+// $producto->mostrarProducto();
+
+
+// echo "<pre>";
+// var_dump($producto);
+// echo "</pre>";
+
+// $producto2 = new Producto('Monitor Curvo', 300, true);
+// $producto2->mostrarProducto();
+
+// echo "<pre>";
+// var_dump($producto2);
+// echo "</pre>";
 
 include 'includes/footer.php';
